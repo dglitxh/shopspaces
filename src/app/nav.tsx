5670281b9bc7260c841b021c15d07b9423a530ca
ydzzy <ydglitch@gmail.com>
2023-07-19 20:51:45 +0000

    upd: nav

--- src/app/nav.tsx
+++ src/app/nav.tsx
@@ -1,20 +1,34 @@
 import React from "react";
 import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
-import { Menu } from "@mui/icons-material";
+import { Menu, Home } from "@mui/icons-material";
 
 export default function MainNav() {
-    return (
-        <div>
-            <AppBar position="static">
-                <Toolbar variant="regular">
-                    <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-                    <Menu />
-                    </IconButton>
-                    <Typography variant="h6" color="inherit" component="div">
-                    Photos
-                    </Typography>
-                </Toolbar>
-                </AppBar>
-        </div>
-    )
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar variant="regular">
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <Menu />
+          </IconButton>
+          <Typography variant="h6" color="inherit" component="div">
+            Photos
+          </Typography>
+          <IconButton
+            edge="end"
+            color="inherit"
+            aria-label="menu"
+            sx={{ ml: 2 }}
+            focusRipple={true}
+          >
+            <Home />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
