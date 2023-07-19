c9e42e005a9fd440184a467b8950acfdb5a63299
ydzzy <ydglitch@gmail.com>
2023-07-19 21:03:07 +0000

    fix: http reqs

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -10,10 +10,10 @@ export const httpReq = async (type: string, url: URL, data: string) => {
     }
     let res
     try {
-        if(type == "GET" || "DELETE") {
+        if(type === "GET" || type === "DELETE") {
             const req = await fetch(url, config)
             res = req.json()
-        }else if (type == "POST" || "PUT") {
+        }else if (type === "POST" || type === "PUT") {
             config["body"] = JSON.stringify(data)
             const req = await fetch(url, config)
             res = req.json()
