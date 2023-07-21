81f53944d4c0115c1dee7045aafd8133ffe1d077
ydzzy <ydglitch@gmail.com>
2023-07-21 16:23:27 +0000

    upg: theming

--- src/utils/theme.tsx
+++ src/utils/theme.tsx
@@ -6,3 +6,29 @@ import { useTheme, ThemeProvider, createTheme } from "@mui/material/styles";
 export const ColorModeContext = React.createContext({
   toggleColorMode: () => {},
 });
+
+export const useThemeHook = () => {
+  const [mode, setMode] = React.useState<"light" | "dark">("dark");
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+      },
+    }),
+    []
+  );
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode]
+  );
+  return {
+    colorMode,
+    theme,
+  };
+};
