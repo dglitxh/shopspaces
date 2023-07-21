97b9e3fbf909adb4b7b06794323f076bc87866b6
ydzzy <ydglitch@gmail.com>
2023-07-21 16:23:02 +0000

    removed context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,9 @@
-"use client"
 import "./globals.css";
 import React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
-import { ColorModeContext } from "@/utils/theme";
+import { ColorModeContext, useThemeHook } from "@/utils/theme";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,32 +17,10 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const [mode, setMode] = React.useState<"light" | "dark">("light");
-  const colorMode = React.useMemo(
-    () => ({
-      toggleColorMode: () => {
-        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
-      },
-    }),
-    []
-  );
-
-  const theme = React.useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode,
-        },
-      }),
-    [mode]
-  );
   return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
+   
         <html lang="en">
           <body className={inter.className}>{children}</body>
         </html>
-      </ThemeProvider>
-    </ColorModeContext.Provider>
   );
 }
