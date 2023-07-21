e7b5baea0d6ce512482fb71dfb524f26b0e17aa6
ydzzy <ydglitch@gmail.com>
2023-07-21 14:17:29 +0000

    add: theme switch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,49 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+"use client"
+import "./globals.css";
+import React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { ColorModeContext } from "@/utils/theme";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'ShopSpaces',
-  description: 'Online marketplace.',
-}
+  title: "ShopSpaces",
+  description: "Online marketplace.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
+  const [mode, setMode] = React.useState<"light" | "dark">("light");
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+      },
+    }),
+    []
+  );
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode]
+  );
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <html lang="en">
+          <body className={inter.className}>{children}</body>
+        </html>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
