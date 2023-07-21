abae67a3a7160ec6d584043d451d0e70a2c7c6bc
ydzzy <ydglitch@gmail.com>
2023-07-21 16:39:01 +0000

    clean

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import "./globals.css";
 import React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import { ColorModeContext, useThemeHook } from "@/utils/theme";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +16,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-        <html lang="en">
-          <body className={inter.className}>{children}</body>
-        </html>
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
   );
 }
