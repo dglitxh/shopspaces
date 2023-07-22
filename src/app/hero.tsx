628f7f1ccbe124ffda09ab47ca64aaeae68584ed
ydzzy <ydglitch@gmail.com>
2023-07-22 12:55:07 +0000

    add: typography

--- src/app/hero.tsx
+++ src/app/hero.tsx
@@ -1,15 +1,19 @@
-import { Container, Grid } from "@mui/material";
+import { Box, Container, Grid, Typography } from "@mui/material";
 import React from "react";
 
 export default function Hero() {
   return (
-    <Container sx={{ height: "100vh", width: "100vw" }}>
-      <Grid sx={{ display: { xs: 12, sm: 12, md: 6, lg: 6 } }}>
-        <h3>Yours Truly the boy</h3>
-      </Grid>
-      <Grid sx={{ display: { xs: 12, sm: 12, md: 6, lg: 6 } }}>
-        <h1>Is Going To School</h1>
-      </Grid>
-    </Container>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <Typography fontSize={120} color={"green"} component={"h1"}>
+        ShopSpaces
+      </Typography>
+    </Box>
   );
 }
