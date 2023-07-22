2da241acf3892094e269d4aa1ee98e0c6c97b833
ydzzy <ydglitch@gmail.com>
2023-07-22 14:22:32 +0000

    upd:

--- src/app/hero.tsx
+++ src/app/hero.tsx
@@ -1,19 +1,47 @@
+"use client";
 import { Box, Container, Grid, Typography } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 import React from "react";
 
 export default function Hero() {
+  useTheme();
   return (
     <Box
       sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
+        background: "rgba(53, 224, 87, 0.043)",
+        borderBottomLeftRadius: 370,
+        borderBottomRightRadius: 370,
       }}
     >
-      <Typography fontSize={120} color={"green"} component={"h1"}>
-        ShopSpaces
-      </Typography>
+      <Box
+        mx={50}
+        p={20}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography fontSize={110} component={"h2"}>
+          <Typography fontSize={100} color={"#00ED64"} component={"span"}>
+            {" "}
+            ShopSpaces.
+          </Typography>
+          Builds virtual stores in minutes
+        </Typography>
+        <Typography
+          fontFamily={"raleway"}
+          component={"p"}
+          fontSize={20}
+          color={"grey"}
+          justifySelf={"center"}
+        >
+          Set up your online shop in a matter of minutes and have the best
+          protection, fairness and insight into your merchandise. Around the
+          clock support is assured.
+        </Typography>
+      </Box>
     </Box>
   );
 }
