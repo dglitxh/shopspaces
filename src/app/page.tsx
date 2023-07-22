9b8d65bc20ea637eb6651b76055c8697fe0b047f
ydzzy <ydglitch@gmail.com>
2023-07-22 12:44:14 +0000

    upd: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import MainNav from "./nav";
 import { ColorModeContext, useThemeHook } from "@/utils/theme";
 import { ThemeProvider } from "@mui/material/styles";
+import Hero from "./hero";
 
 export default function Home() {
   const theme = useThemeHook();
@@ -12,6 +13,7 @@ export default function Home() {
       <ThemeProvider theme={theme.theme}>
         <div>
           <MainNav />
+          <Hero />
           <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex"></div>
           </main>
