5f24b064ddcaaf1814f51192ce0143a5095deed6
ydzzy <ydglitch@gmail.com>
2023-07-22 14:23:38 +0000

    add: css baseline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import Image from "next/image";
+import { CssBaseline } from "@mui/material";
 import React from "react";
 import MainNav from "./nav";
 import { ColorModeContext, useThemeHook } from "@/utils/theme";
@@ -11,11 +11,12 @@ export default function Home() {
   return (
     <ColorModeContext.Provider value={theme.colorMode}>
       <ThemeProvider theme={theme.theme}>
+        <CssBaseline />
         <div>
           <MainNav />
           <Hero />
           <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex"></div>
+            <div className="z-10 w-full max-w-5xl items-center justify-between text-sm lg:flex"></div>
           </main>
         </div>
       </ThemeProvider>
