ad861d19055e198f5e7daed8cf145f9d15073ec4
ydzzy <ydglitch@gmail.com>
2023-07-23 20:16:51 +0000

    add: responsive viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0"
+      ></meta>
       <body className={raleway.className}>{children}</body>
     </html>
   );
