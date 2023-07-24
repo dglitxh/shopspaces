8718b6c062a6a203d9a43df43bde264a104e43bc
ydzzy <ydglitch@gmail.com>
2023-07-24 21:30:15 +0000

    cleanup: imports

--- src/app/hero.tsx
+++ src/app/hero.tsx
@@ -1,15 +1,13 @@
 "use client";
 import { Box, Container, Typography } from "@mui/material";
-import { useTheme } from "@mui/material/styles";
 import React from "react";
 
 export default function Hero() {
-  useTheme();
   return (
     <Container maxWidth="lg">
       <Box
         sx={{
-          background: "rgba(53, 224, 87, 0.043)",
+          // background: "rgba(53, 224, 87, 0.043)",
           borderBottomLeftRadius: 370,
           borderTopRightRadius: 370,
           fontFamily: "Raleway",
@@ -39,7 +37,12 @@ export default function Hero() {
             builds your virtual stores in a flash.
           </Typography>
 
-          <Typography component={"p"} color={"grey"} fontSize={"22px"} maxWidth={"sm"}>
+          <Typography
+            component={"p"}
+            color={"grey"}
+            fontSize={"22px"}
+            maxWidth={"sm"}
+          >
             Set up your online shop in a matter of minutes and have the best
             protection, fairness and insight into your merchandise. Around the
             clock support is assured.
