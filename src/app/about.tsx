4f15dd7254b303d4127ba5f090aa0dc306288853
Ydeezly <74550749+dglitxh@users.noreply.github.com>
2023-08-31 01:00:30 +0000

    fix

--- src/app/about.tsx
+++ src/app/about.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { Box, Container, Grid } from "@mui/"
 
-export function about () {
+export function About () {
     return (
         <Container>
             <Grid container>
