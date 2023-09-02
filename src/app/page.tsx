a956dc0c967e180d88f0af32485909f4937ed541
Ydeezly <74550749+dglitxh@users.noreply.github.com>
2023-09-02 13:55:29 +0000

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,8 +18,8 @@ export default function Home() {
           <main className="flex min-h-screen flex-col items-center justify-between ">
             <div className="z-10 w-full  items-center justify-between text-xl lg:flex">
               <Hero />
-              <About />
             </div>
+            <About />
           </main>
         </div>
       </ThemeProvider>
