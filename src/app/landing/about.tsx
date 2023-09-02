b3c61c1f99d68876b5605c18315d93e796d52a33
Ydeezly <74550749+dglitxh@users.noreply.github.com>
2023-09-02 13:02:05 +0000

    upd: about

--- src/app/landing/about.tsx
+++ src/app/landing/about.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { Box, Container, Grid } from "@mui/material";
+import aboutData from "@utils/data";
 
 export function About() {
   return (
     <Container>
       <Grid container>
-        <Grid item></Grid>
-        <Grid item></Grid>
+        {data.map((el) => (
+          <Box id={el.id}>
+            <Grid item>
+              <Typography variant={"h2"} color="green">aboutData.title</Typography>
+              <Typography variant="p">aboutData.text</Typography>
+            </Grid>
+            <Grid item></Grid>
+          </Box>
+        ))}
       </Grid>
     </Container>
   );
