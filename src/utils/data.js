db06747188b59ca4a4eb04fd76c89dcca8823466
Ydeezly <74550749+dglitxh@users.noreply.github.com>
2023-09-02 10:27:47 +0000

    add: data wai

---
+++ src/utils/data.js
@@ -0,0 +1,16 @@
+export default aboutData = [
+  {
+    id: 1,
+    title: `Your shop, Your rules.`,
+    text: `Craft your unique storefront that reflects your brand identity.
+                   With our user-friendly tools, you're just a few clicks away from designing a shop that stands out`,
+    svg: "",
+  },
+  {
+    id: 2,
+    title: "Showcase your creations",
+    text: `Display your products in all their glory. From handmade crafts to trendy fashion
+          let your items shinen and captivate your customers' attention.`,
+    svg: "",
+  },
+];
