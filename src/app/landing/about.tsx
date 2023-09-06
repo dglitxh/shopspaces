9a8df551ca85b802f7659c599fa26b804189cfd3
Ydeezly <74550749+dglitxh@users.noreply.github.com>
2023-09-06 14:40:39 +0000

    fix: imports

--- src/app/landing/about.tsx
+++ src/app/landing/about.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Image from 'next/image'
-import { Box, Container, Grid, Typography, Item } from "@mui/material";
+import Image from "next/image";
+import { Box, Container, Grid, Typography} from "@mui/material";
 import { aboutData } from "@/utils/data";
 
 export default function About() {
@@ -12,19 +12,14 @@ export default function About() {
             <Grid item xs={12}>
               <Item>
                 <Typography variant={"h2"} color="green">
-                {el.title}
-              </Typography>
+                  {el.title}
+                </Typography>
               </Item>
-              
+
               <Typography component={"p"}>{el.text}</Typography>
             </Grid>
             <Grid item xs={12}>
-            <Image
-              src={el.img}
-              width={500}
-              height={500}
-              alt="grid pic"
-            />
+              <Image src={el.img} width={500} height={500} alt="grid pic" />
             </Grid>
           </Box>
         ))}
