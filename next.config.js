ca8f91a587234a6d1e0cbb8a6443f4c139dae55e
Ydeezly <74550749+dglitxh@users.noreply.github.com>
2023-09-07 11:59:00 +0000

    upd: conf

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "images.pexel.com",
+        hostname: "images.pexels.com",
         port: "",
       },
     ],
