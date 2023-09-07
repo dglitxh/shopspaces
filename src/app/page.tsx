269589f10a27531b71cfd69c46a77eec7e6f4cbd
Ydeezly <74550749+dglitxh@users.noreply.github.com>
2023-09-07 11:59:10 +0000

    upd: page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,9 +16,8 @@ export default function Home() {
         <div>
           <MainNav />
           <main className="flex min-h-screen flex-col items-center justify-between ">
-            <div className="z-10 w-full  items-center justify-between text-xl lg:flex">
-              <Hero />
-            </div>
+            <Hero />
+
             <About />
           </main>
         </div>
