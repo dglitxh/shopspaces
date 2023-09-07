73b9d9f017d85193cd9077da3cee3a197297a4d1
Ydeezly <74550749+dglitxh@users.noreply.github.com>
2023-09-07 11:06:56 +0000

    upd: conf

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexel.com",
+        port: "",
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig
