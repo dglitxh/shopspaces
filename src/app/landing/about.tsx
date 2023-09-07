716261f7472ace219204c2011c74368d7108412d
Ydeezly <74550749+dglitxh@users.noreply.github.com>
2023-09-07 11:59:17 +0000

    upd: abt

--- src/app/landing/about.tsx
+++ src/app/landing/about.tsx
@@ -5,22 +5,22 @@ import { aboutData } from "@/utils/data";
 
 export default function About() {
   return (
-    <Container maxWidth="lg">
-      <Grid container>
+    <Container maxWidth="lg" sx={{ flexGrow: 1 }}>
+      <Box p={"10%"}>
         {aboutData.map((el) => (
-          <Box key={el.id}>
-            <Grid item xs={12}>
+          <Grid container spacing={2} key={el.id} p={"5%"}>
+            <Grid item lg={6} xl={6} md={6}>
               <Typography variant={"h2"} color={"#00ED64"}>
                 {el.title}
               </Typography>
               <Typography component={"p"}>{el.text}</Typography>
             </Grid>
-            <Grid item xs={12}>
-              <Image src={el.img} width={500} height={500} alt="grid pic" />
+            <Grid item xs={12} lg={6} xl={6} md={6}>
+              <Image src={el.img} width={300} height={300} alt="grid pic" />
             </Grid>
-          </Box>
+          </Grid>
         ))}
-      </Grid>
+      </Box>
     </Container>
   );
 }
