f1fa42accf56c445ee6e71b21c4d2661568ca6a1
ydzly <ydglitch@gmail.com>
2023-09-26 14:53:06 +0000

    upd: about

--- src/app/landing/about.tsx
+++ src/app/landing/about.tsx
@@ -1,9 +1,28 @@
+"use client";
 import React from "react";
 import Image from "next/image";
-import { Box, Container, Grid, Typography } from "@mui/material";
+import { Box, Container, Grid, Typography, Button } from "@mui/material";
 import { aboutData } from "@/utils/data";
 
 export default function About() {
+  const RandCol = (props: any) => {
+    let title: string = props.title;
+    let arr = title.split(" ");
+    let r = Math.floor(Math.random() * arr.length);
+    const partA = arr.slice(0, r).join(" ") + " ";
+    const partB = " " + arr.slice(r + 1).join(" ");
+    let colored = arr[r];
+    console.log(partA, colored + "col", partB);
+    return (
+      <Typography fontSize={"110%"} variant={"h3"}>
+        {partA}
+        <Typography color={"primary"} component="span" fontSize={"110%"}>
+          {colored}
+        </Typography>
+        {partB}
+      </Typography>
+    );
+  };
   return (
     <Container maxWidth="lg" sx={{ flexGrow: 1 }}>
       <Box p={"10%"}>
@@ -21,8 +40,15 @@ export default function About() {
                 flexDirection: "column",
               }}
             >
-              <Typography variant={"h2"}>{el.title}</Typography>
-              <Typography component={"p"}>{el.text}</Typography>
+              <Typography variant={"h3"}>
+                <RandCol title={el.title} />
+              </Typography>
+              <Typography component={"p"} color="gray">
+                {el.text}
+                <br></br>
+                <br></br>
+                <Button variant="outlined">Learn more</Button>
+              </Typography>
             </Grid>
             <Grid item xs={12} lg={6} xl={6} md={6}>
               <Image src={el.img} width={300} height={300} alt="grid pic" />
