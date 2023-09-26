f71e3a6ef79340cff4a2e6bd1676c1b1d8a87f2d
ydzly <ydglitch@gmail.com>
2023-09-26 12:22:08 +0000

    add: font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,14 @@ export default function RootLayout({
         name="viewport"
         content="width=device-width, initial-scale=1.0"
       ></meta>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Raleway:wght@400&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body className={raleway.className}>{children}</body>
     </html>
   );
