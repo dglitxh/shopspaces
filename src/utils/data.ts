7798383ffd39b1ce00a4630d0c274c6696640903
ydzly <ydglitch@gmail.com>
2023-09-27 10:02:59 +0000

    upd: data

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,4 +1,6 @@
 import ShopSvg from "@/assets/shop";
+import GrowthSvg from "@/assets/growth";
+import IdeaSvg from "@/assets/idea";
 
 export const aboutData = [
   {
@@ -13,7 +15,7 @@ export const aboutData = [
     title: "Showcase your creations",
     text: `Display your products in all their glory. From handmade crafts to trendy fashion
           let your items shinen and captivate your customers' attention.`,
-    img: ShopSvg(),
+    img: IdeaSvg(),
   },
   {
     id: 3,
@@ -21,6 +23,6 @@ export const aboutData = [
     text: `Break geographical barriers and sell to customers around the world. 
     Our platform's robust features ensure a seamless shopping experience,
      no matter where your customers are.`,
-    img: ShopSvg(),
+    img: GrowthSvg(),
   },
 ];
