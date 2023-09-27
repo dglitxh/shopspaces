29cbc93e13f812a22f2522478479bf758f682669
ydzly <ydglitch@gmail.com>
2023-09-27 09:51:03 +0000

    upd: data

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,20 +1,26 @@
+import ShopSvg from "@/assets/shop";
+
 export const aboutData = [
   {
     id: 1,
-    title: `Your shop, Your rules.`,
+    title: `Your shop Your rules`,
     text: `Craft your unique storefront that reflects your brand identity.
                    With our user-friendly tools, you're just a few clicks away from designing a shop that stands out`,
-    img: "https://images.pexels.com/photos/4482900/pexels-photo-4482900.jpeg?auto=compress&cs=tinysrgb&w=600",
+    img: ShopSvg(),
   },
   {
     id: 2,
     title: "Showcase your creations",
     text: `Display your products in all their glory. From handmade crafts to trendy fashion
           let your items shinen and captivate your customers' attention.`,
-    img: "https://images.pexels.com/photos/4482900/pexels-photo-4482900.jpeg?auto=compress&cs=tinysrgb&w=600",
+    img: ShopSvg(),
   },
-  //   {
-  //     id: 3,
-  //     title:
-  //   }
+  {
+    id: 3,
+    title: "Reach the world",
+    text: `Break geographical barriers and sell to customers around the world. 
+    Our platform's robust features ensure a seamless shopping experience,
+     no matter where your customers are.`,
+    img: ShopSvg(),
+  },
 ];
