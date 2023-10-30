5ba73b9170679fbe16294ec3fc8cfd17c4b7ea53
ydeezly <ydglitch@gmail.com>
2023-10-30 21:19:23 +0000

    upd: http helpers

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,28 +1,27 @@
-
 export const httpReq = async (type: string, url: string, data: string) => {
-    // an empty string is used for requests without data
-    type = type.toUpperCase()
-    let config: RequestInit = {
-        method: type,
-        headers: {
-            "Accept": "application/json",
-            "Content-Type": "application/json"
-        },
+  // an empty string is used for requests without data
+  type = type.toUpperCase();
+  let config: RequestInit = {
+    method: type,
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  };
+  let res;
+  try {
+    if (type === "GET" || type === "DELETE") {
+      const req = await fetch(url, config);
+      res = req.json();
+    } else if (type === "POST" || type === "PUT") {
+      config["body"] = JSON.stringify(data);
+      const req = await fetch(url, config);
+      res = req.json();
     }
-    let res
-    try {
-        if(type === "GET" || type === "DELETE") {
-            const req = await fetch(url, config)
-            res = req.json()
-        }else if (type === "POST" || type === "PUT") {
-            config["body"] = JSON.stringify(data)
-            const req = await fetch(url, config)
-            res = req.json()
-        }
-        return res
-    }catch (e) {
-        console.log(e)
-        return e
-    }
-}
-
+    console.log(res);
+    return res;
+  } catch (e) {
+    console.log(e);
+    return e;
+  }
+};
