cac3083dc957cd28142bf7ff832a8bf279cf4cde
ydeezly <ydglitch@gmail.com>
2023-10-30 19:43:29 +0000

    add: prod page

---
+++ src/app/mystore/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+import Catalog from "./catalog";
+
+export default function Home() {
+  return (
+    <div>
+      <main className="flex min-h-screen flex-col items-center justify-between ">
+        WE THE BEST.
+        <Catalog />
+      </main>
+    </div>
+  );
+}
