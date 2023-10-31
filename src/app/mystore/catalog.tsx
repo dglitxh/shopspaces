6db06d4e8418f36fc5d179094ed2ae6be90e0599
ydeezly <ydglitch@gmail.com>
2023-10-31 19:34:00 +0000

    fix: catalog

--- src/app/mystore/catalog.tsx
+++ src/app/mystore/catalog.tsx
@@ -13,43 +13,45 @@ import { httpReq } from "@/utils/helpers";
 import { useProductStore } from "@/utils/data";
 
 export default function Catalog() {
-  let products: Array<any> = [];
+  let [products, setProducts] = useState<Array<string>>([]);
   useEffect(() => {
+    console.log("ahnnn ei");
     const getProds = async () => {
-      const prods = await httpReq("GET", `${BACKEND}/products`, "");
-      console.log(prods);
-      products = prods;
+      const prods = await httpReq("GET", `${BACKEND}/stores/1/products`, "");
+      console.log(prods, "prosdffgh");
+      setProducts(prods);
     };
     getProds();
   }, []);
-  useProductStore((state: any) => state.addProducts(products));
-  let items = useProductStore((state: any) => state.items);
+  // useProductStore((state: any) => state.addProducts(products));
+  // let items = useProductStore((state: any) => state.items);
 
   return (
     <Container>
       <Typography> My Catalog</Typography>
-      {items.map((el: any) => (
-        <Card sx={{ maxWidth: 345, margin: 1 }} key={el.id}>
-          {/* <CardMedia
+      {products.length > 1 &&
+        products.map((el: any) => (
+          <Card sx={{ maxWidth: 345, margin: 1 }} key={el.id}>
+            {/* <CardMedia
             sx={{ height: 40 }}
             image="https://www.svgrepo.com/show/530443/interface-control.svg"
             title="green iguana"
           /> */}
 
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              ( el.name)
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              {el}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small">Share</Button>
-            <Button size="small">Learn More</Button>
-          </CardActions>
-        </Card>
-      ))}
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {el.name}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {el.description}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">View</Button>
+              <Button size="small">Add to cart</Button>
+            </CardActions>
+          </Card>
+        ))}
     </Container>
   );
 }
