456c482eed57a6c9f0d6c117b01816c06778f570
ydzly <ydglitch@gmail.com>
2023-11-02 15:41:14 +0000

    add: page

---
+++ src/app/dash/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Dashboard from "./Dashboard";
+
+export default function Home() {
+  return (
+    <div>
+      <main className="flex min-h-screen flex-col items-center justify-between ">
+        WE THE BEST.
+        <Dashboard />
+      </main>
+    </div>
+  );
+}
