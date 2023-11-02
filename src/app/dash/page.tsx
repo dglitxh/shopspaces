de5b12b238b0c68d1fe75f38fd523ef6ce49f659
ydzly <ydglitch@gmail.com>
2023-11-02 16:21:40 +0000

    upd: dash page

--- src/app/dash/page.tsx
+++ src/app/dash/page.tsx
@@ -1,13 +1,20 @@
+"use client";
 import React from "react";
 import Dashboard from "./Dashboard";
+import { ColorModeContext, useThemeHook } from "@/utils/theme";
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
 
 export default function Home() {
+  const { theme, colorMode } = useThemeHook();
   return (
-    <div>
-      <main className="flex min-h-screen flex-col items-center justify-between ">
-        WE THE BEST.
-        <Dashboard />
-      </main>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div>
+          <Dashboard />
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
