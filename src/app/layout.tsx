1e9562881db402975cf02b1352608d019cf4f2f6
ydzly <ydglitch@gmail.com>
2023-11-03 13:04:41 +0000

    upd: font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Raleway:wght@400&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Raleway:wght@450&display=swap"
           rel="stylesheet"
         />
       </head>
