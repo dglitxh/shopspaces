2d95ad3ac6d888849698c14750fa1acff68fd846
ydzly <ydglitch@gmail.com>
2023-11-09 11:04:02 +0000

    upd: dash theme

--- src/app/dash/page.tsx
+++ src/app/dash/page.tsx
@@ -1,20 +1,9 @@
 "use client";
 import React from "react";
 import Dashboard from "./Dashboard";
-import { ColorModeContext, useThemeHook } from "@/utils/theme";
-import { CssBaseline } from "@mui/material";
-import { ThemeProvider } from "@mui/material/styles";
 
 export default function Home() {
-  const { theme, colorMode } = useThemeHook();
   return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <div>
           <Dashboard />
-        </div>
-      </ThemeProvider>
-    </ColorModeContext.Provider>
   );
 }
