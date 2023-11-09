5a40d92fa2624ea9b5203d9d663daaa9c1f655dc
ydzly <ydglitch@gmail.com>
2023-11-09 11:03:08 +0000

    upd: landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 import { CssBaseline, Container } from "@mui/material";
 import React from "react";
 import { ColorModeContext, useThemeHook } from "@/utils/theme";
-import { ThemeProvider } from "@mui/material/styles";
 import Hero from "./landing/hero";
 import MainNav from "./landing/nav";
 import MediaCards from "./landing/cards";
@@ -10,21 +9,15 @@ import Pricing from "./landing/pricing";
 import About from "./landing/about";
 
 export default function Home() {
-  const { theme, colorMode } = useThemeHook();
   return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Container>
-          <MainNav />
-          <main className="flex min-h-screen flex-col items-center justify-between ">
-            <Hero />
-            <About />
-            <MediaCards />
-            <Pricing />
-          </main>
-        </Container>
-      </ThemeProvider>
-    </ColorModeContext.Provider>
+    <Container>
+      <MainNav />
+      <main className="flex min-h-screen flex-col items-center justify-between ">
+        <Hero />
+        <About />
+        <MediaCards />
+        <Pricing />
+      </main>
+    </Container>
   );
 }
