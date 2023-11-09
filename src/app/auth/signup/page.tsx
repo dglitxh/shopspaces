0226aae11c90069378d18f501a3827712cd9ce91
ydzly <ydglitch@gmail.com>
2023-11-09 11:03:41 +0000

    upd: signup

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,20 +1,11 @@
 "use client";
 import React from "react";
 import SignUp from "./signup";
-import { ColorModeContext, useThemeHook } from "@/utils/theme";
-import { CssBaseline } from "@mui/material";
-import { ThemeProvider } from "@mui/material/styles";
 
 export default function Home() {
-  const { theme, colorMode } = useThemeHook();
   return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
         <div>
           <SignUp />
         </div>
-      </ThemeProvider>
-    </ColorModeContext.Provider>
   );
 }
