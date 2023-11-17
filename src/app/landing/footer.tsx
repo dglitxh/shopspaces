aaca85fbb3dfe408b0d2e0e92c901da9cc86cb5c
ydzly <ydglitch@gmail.com>
2023-11-17 10:18:16 +0000

    upd: footer

--- src/app/landing/footer.tsx
+++ src/app/landing/footer.tsx
@@ -14,7 +14,7 @@ interface FooterColumn {
 
 const columns: FooterColumn[] = [
   {
-    title: SITENAME,
+    title: "Social",
     links: [
       {
         title: "Twitter",
@@ -83,54 +83,21 @@ const Footer: React.FC = () => {
             </a>
           </div>
           <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
-            <div>
-              <h2 className="mb-6 font-semibold">Resources</h2>
-              <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                <li className="mb-4">
-                  <a href="#" className="hover:underline">
-                    {SITENAME}
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://tailwindcss.com/"
-                    className="hover:underline"
-                  >
-                    Tailwind CSS
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h2 className="mb-6 text-sm font-semibold">Follow us</h2>
-              <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                <li className="mb-4">
-                  <a href="#" className="hover:underline ">
-                    Github
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="hover:underline">
-                    Discord
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h2 className="mb-6 text-sm font-semibold">Legal</h2>
-              <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                <li className="mb-4">
-                  <a href="#" className="hover:underline">
-                    Privacy Policy
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="hover:underline">
-                    Terms &amp; Conditions
-                  </a>
-                </li>
-              </ul>
-            </div>
+            {columns.map((col) => (
+              <div>
+                <h2 className="mb-6 font-semibold">{col.title}</h2>
+                <ul className="text-gray-700 dark:text-gray-400 font-medium">
+                  {col.links &&
+                    col.links.map((lnk) => (
+                      <li className="mb-4">
+                        <a href={lnk.href} className="hover:underline">
+                          {lnk.title}
+                        </a>
+                      </li>
+                    ))}
+                </ul>
+              </div>
+            ))}
           </div>
         </div>
         <hr className="my-2 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
