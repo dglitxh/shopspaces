8126ecc0837c4552e1d63356b2a5f5deb2c15d9a
ydzly <ydglitch@gmail.com>
2023-11-17 11:27:45 +0000

    fix: theming

--- src/app/auth/signin/SignIn.tsx
+++ src/app/auth/signin/SignIn.tsx
@@ -44,7 +44,7 @@ export default function SignIn() {
   };
 
   return (
-    <Container component="main" maxWidth="xs">
+    <Container component="main" maxWidth="xs" sx={{my: "5%"}}>
       <CssBaseline />
       <Box
         sx={{
@@ -54,18 +54,18 @@ export default function SignIn() {
           alignItems: "center",
         }}
       >
-        <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-full  p-4 border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:border-gray-700">
           <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
           <form className="space-y-6" action="submit" onSubmit={handleSubmit}>
-            <h5 className="text-xl font-medium text-gray-900 dark:text-white">
+            <h5 className="text-xl font-medium">
               Sign In
             </h5>
             <div>
               <label
                 // for="email"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                className="block mb-2 text-sm font-medium"
               >
                 Your email
               </label>
@@ -73,7 +73,7 @@ export default function SignIn() {
                 type="email"
                 name="email"
                 id="email"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                className=" border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-500 dark:placeholder-gray-400"
                 placeholder="name@company.com"
                 required
               />
@@ -81,7 +81,7 @@ export default function SignIn() {
             <div>
               <label
                 // for="password"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                className="block mb-2 text-sm font-medium"
               >
                 Your password
               </label>
@@ -90,7 +90,7 @@ export default function SignIn() {
                 name="password"
                 id="password"
                 placeholder="••••••••"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                className=" border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-500 dark:placeholder-gray-400"
                 required
               />
             </div>
@@ -101,13 +101,13 @@ export default function SignIn() {
                     id="remember"
                     type="checkbox"
                     value=""
-                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+                    className="w-4 h-4 border border-gray-300 rounded  focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
                     required
                   />
                 </div>
                 <label
                   // for="remember"
-                  className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                  className="ml-2 text-sm font-medium dark:text-gray-300"
                 >
                   Remember me
                 </label>
