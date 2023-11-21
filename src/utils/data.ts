a5064c91884876822a4df4c4f3fa743702e0b21f
ydzly <ydglitch@gmail.com>
2023-11-21 15:56:44 +0000

    upd: added stock data

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -65,3 +65,148 @@ export const cardData = [
     text: "Keep tack of orders, manage inventory, and update product listing with ease. Spend less time worrying about technicalities and more time doing what you love.",
   },
 ];
+
+export const productsData = [
+  {
+    id: 1,
+    name: "Laptop",
+    brand: "XYZ",
+    price: 999.99,
+    quantity: 50,
+    category: "Electronics",
+  },
+  {
+    id: 2,
+    name: "Smartphone",
+    brand: "ABC",
+    price: 599.99,
+    quantity: 100,
+    category: "Electronics",
+  },
+  {
+    id: 3,
+    name: "Running Shoes",
+    brand: "Nike",
+    price: 79.99,
+    quantity: 200,
+    category: "Footwear",
+  },
+  {
+    id: 4,
+    name: "Backpack",
+    brand: "Adidas",
+    price: 49.99,
+    quantity: 75,
+    category: "Fashion",
+  },
+  {
+    id: 5,
+    name: "Digital Camera",
+    brand: "Canon",
+    price: 449.99,
+    quantity: 30,
+    category: "Electronics",
+  },
+  {
+    id: 6,
+    name: "T-shirt",
+    brand: "Gap",
+    price: 19.99,
+    quantity: 150,
+    category: "Fashion",
+  },
+  {
+    id: 7,
+    name: "Coffee Maker",
+    brand: "Keurig",
+    price: 129.99,
+    quantity: 40,
+    category: "Appliances",
+  },
+  {
+    id: 8,
+    name: "Gaming Console",
+    brand: "Sony",
+    price: 299.99,
+    quantity: 60,
+    category: "Electronics",
+  },
+  {
+    id: 9,
+    name: "Watch",
+    brand: "Casio",
+    price: 89.99,
+    quantity: 90,
+    category: "Accessories",
+  },
+  {
+    id: 10,
+    name: "Blender",
+    brand: "Hamilton Beach",
+    price: 39.99,
+    quantity: 25,
+    category: "Appliances",
+  },
+];
+
+
+export const pricingCats = [
+  {
+    name: "Starter",
+    description: "Ideal for small businesses getting started",
+    price: {
+      monthly: 29.99,
+      annual: 299.99,
+    },
+    features: [
+      "Basic Store Setup",
+      "Limited Product Listings",
+      "Standard Support",
+    ],
+  },
+  {
+    name: "Standard",
+    description: "Great for growing businesses with moderate needs",
+    price: {
+      monthly: 59.99,
+      annual: 599.99,
+    },
+    features: [
+      "Unlimited Product Listings",
+      "Advanced Analytics",
+      "Payment Processing",
+      "Priority Support",
+      "Mobile Optimization",
+      "Priority Support",
+    ],
+  },
+  {
+    name: "Business",
+    description: "Perfect for established businesses with high volume",
+    price: {
+      monthly: 99.99,
+      annual: 999.99,
+    },
+    features: [
+      "Customizable Themes",
+      "Abandoned Cart Recovery",
+      "API Access",
+      "Dedicated Account Manager",
+      "Custom Domain",
+      "24/7 Support",
+    ],
+  },
+  // {
+  //   name: "Enterprise",
+  //   description: "Tailored solutions for large-scale enterprises",
+  //   price: {
+  //     contactSales: true,
+  //   },
+  //   features: [
+  //     "Custom Development",
+  //     "Multi-Store Management",
+  //     "24/7 Priority Support",
+  //     "Service Level Agreement (SLA)",
+  //   ],
+  // },
+];
