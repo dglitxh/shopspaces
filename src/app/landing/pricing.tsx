3fde28446df1d78d5b06bd3e6e29246b60d55cbe
ydzly <ydglitch@gmail.com>
2023-11-21 15:55:53 +0000

    upd: added pricing packages

--- src/app/landing/pricing.tsx
+++ src/app/landing/pricing.tsx
@@ -5,9 +5,11 @@ import {
   Paper,
   Button,
   Typography,
+  Box,
 } from "@mui/material";
 import React from "react";
 import dynamic from "next/dynamic";
+import { pricingCats } from "@/utils/data";
 
 const ColHeading = dynamic(() => import("../components/colHeading"), {
   ssr: false,
@@ -26,9 +28,9 @@ export default function Pricing() {
         <ColHeading title={"Great value for money"} size={"115%"} />
       </Typography>
       <Grid container spacing={8}>
-        {[1, 2, 3].map((el: any) => (
-          <Grid key={el} item xs={12} sm={12} md={4} lg={4} xl={4}>
-            <PricingCard />
+        {pricingCats.map((pkg: object) => (
+          <Grid key={String(Math.random())} item xs={12} sm={12} md={4} lg={4} xl={4}>
+            <PricingCard pkg={pkg} />
           </Grid>
         ))}
       </Grid>
@@ -36,8 +38,9 @@ export default function Pricing() {
   );
 }
 
-function PricingCard() {
+function PricingCard(props: any) {
   const theme = useTheme();
+  const { pkg } = props;
   return (
     <Paper
       sx={{
@@ -47,115 +50,36 @@ function PricingCard() {
       className="w-full max-w-sm p-4 border border-gray-200 rounded-lg shadow sm:p-8 dark:border-gray-700"
     >
       <h5
-        style={{ color: theme.palette.secondary.main, fontWeight: "bold" }}
+        style={{
+          color: theme.palette.secondary.main,
+          fontWeight: "bold",
+        }}
         className="mb-4 text-xl font-medium"
       >
-        Standard plan
+        {pkg.name}
       </h5>
       <div className="flex items-baseline">
         <span className="text-3xl font-semibold">$</span>
-        <span className="text-5xl font-extrabold tracking-tight">49</span>
+        <span className="text-5xl font-extrabold tracking-tight">
+          {pkg.price.monthly}
+        </span>
         <span className="ml-1 text-xl font-normal">/month</span>
       </div>
       <ul role="list" className="space-y-5 my-7">
-        <li className="flex space-x-3 items-center">
-          <svg
-            className="flex-shrink-0 w-4 h-4 text-blue-600"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill={theme.palette.secondary.main}
-            viewBox="0 0 20 20"
-          >
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-          </svg>
-          <span className="text-base font-normal leading-tight">
-            2 team members
-          </span>
-        </li>
-        <li className="flex space-x-3">
-          <svg
-            className="flex-shrink-0 w-4 h-4 text-blue-600"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill={theme.palette.secondary.main}
-            viewBox="0 0 20 20"
-          >
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-          </svg>
-          <span className="text-base font-normal leading-tight">
-            20GB Cloud storage
-          </span>
-        </li>
-        <li className="flex space-x-3">
-          <svg
-            className="flex-shrink-0 w-4 h-4 text-blue-600"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill={theme.palette.secondary.main}
-            viewBox="0 0 20 20"
-          >
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-          </svg>
-          <span className="text-base font-normal leading-tight">
-            Integration help
-          </span>
-        </li>
-        <li className="flex space-x-3 line-through decoration-gray-500">
-          <svg
-            className="flex-shrink-0 w-4 h-4"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill={theme.palette.secondary.main}
-            viewBox="0 0 20 20"
-          >
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-          </svg>
-          <span className="text-base font-normal leading-tight">
-            Sketch Files
-          </span>
-        </li>
-        <li className="flex space-x-3 line-through decoration-gray-500">
-          <svg
-            className="flex-shrink-0 w-4 h-4"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill={theme.palette.secondary.main}
-            viewBox="0 0 20 20"
-          >
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-          </svg>
-          <span className="text-base font-normal leading-tight">
-            API Access
-          </span>
-        </li>
-        <li className="flex space-x-3 line-through decoration-gray-500">
-          <svg
-            className="flex-shrink-0 w-4 h-4"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill={theme.palette.secondary.main}
-            viewBox="0 0 20 20"
-          >
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-          </svg>
-          <span className="text-base font-normal leading-tight">
-            Complete documentation
-          </span>
-        </li>
-        <li className="flex space-x-3 line-through decoration-gray-500">
-          <svg
-            className="flex-shrink-0 w-4 h-4"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill={theme.palette.secondary.main}
-            viewBox="0 0 20 20"
-          >
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-          </svg>
-          <span className="text-base font-normal leading-tight">
-            24×7 phone & email support
-          </span>
-        </li>
+        {pkg.features.map((ft: string) => (
+          <li className="flex space-x-3 items-center">
+            <svg
+              className="flex-shrink-0 w-4 h-4 text-blue-600"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill={theme.palette.secondary.main}
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+            </svg>
+            <span className="text-base font-normal leading-tight">{ft}</span>
+          </li>
+        ))}
       </ul>
       <Button variant="contained" color="primary">
         Choose plan
